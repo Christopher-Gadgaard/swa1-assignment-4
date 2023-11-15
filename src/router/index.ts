7e12088d2da9ef487b78f1adb29c1955d1e9331d
Vlad Lazar <lazar.vlad151@gmail.com>
2023-11-15 17:59:41 +0100

    created the views structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,37 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-
+import HomeView from "../views/homeView/HomeView.vue";
+import LoginView from "../views/loginView/LoginView.vue";
+import ProfileView from "../views/profileView/ProfileView.vue";
+import GameView from "../views/gameView/GameView.vue";
+// import ScoreBoard from "../views/ScoreBoardView.vue";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: HomeView,
   },
+
+
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
+  {
+    path: "/profile",
+    name: "profile",
+    component: ProfileView,
+  },
+  {
+    path: "/game",
+    name: "game",
+    component: GameView,
+  },
+  // {
+  //   path: "/high-scores",
+  //   name: "highScores",
+  //   component: ScoreBoard,
+  // }
 ];
 
 const router = createRouter({
