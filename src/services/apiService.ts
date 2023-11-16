b310c3ffde94096e71fba9f89e6eddf2fd5052d4
Chris309283 <Christopher.holmelund@gmail.com>
2023-11-16 19:56:33 +0100

    apiService made

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,1 +1,45 @@
-//apiService.ts
+const BASE_URL = 'http://localhost:9090';
+
+export const apiService = {
+  async get(endpoint: string, token: string) {
+    const response = await fetch(`${BASE_URL}${endpoint}?token=${token}`, {
+      method: 'GET',
+      headers: this.buildHeaders(token)
+    });
+    return this.processResponse(response);
+  },
+
+  async post(endpoint: string, token: string, body?: object) {
+    const response = await fetch(`${BASE_URL}${endpoint}?token=${token}`, {
+      method: 'POST',
+      headers: this.buildHeaders(token),
+      body: body ? JSON.stringify(body) : null
+    });
+    return this.processResponse(response);
+  },
+
+  async patch(endpoint: string, token: string, body: object) {
+    const response = await fetch(`${BASE_URL}${endpoint}?token=${token}`, {
+      method: 'PATCH',
+      headers: this.buildHeaders(token),
+      body: JSON.stringify(body)
+    });
+    return this.processResponse(response);
+  },
+
+  buildHeaders(token: string) {
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+      // 'Authorization': `Bearer ${token}` // Uncomment if you switch to using Bearer tokens
+    });
+    return headers;
+  },
+
+  async processResponse(response: Response) {
+    if (!response.ok) {
+      const message = await response.text();
+      throw new Error(message || 'Server responded with an error');
+    }
+    return response.json();
+  }
+};
