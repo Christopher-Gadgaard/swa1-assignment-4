6401f2426a49c53a0474d22d3609205a2a0af7b9
Chris309283 <Christopher.holmelund@gmail.com>
2023-11-16 15:11:31 +0100

    gameboard and tiles fixed

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,1 +1,85 @@
 <!--Gamebaord.vue-->
+<template>
+  <div class="game-board">
+    <div v-for="(row, rowIndex) in boardData" :key="rowIndex" class="board-row">
+      <GameTile
+        v-for="(tile, colIndex) in row"
+        :key="colIndex"
+        :tileImage="getTileImage(tile)"
+        :position="{ row: rowIndex, col: colIndex }"
+        @tile-clicked="handleTileClick"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue';
+import GameTile, { TilePosition } from './GameTile.vue';
+
+// Define available tile types based on your SVG filenames
+const tileTypes = ['astronaut', 'brain-slug', 'galaxy', 'laser-gun', 'millennium-falcon', 'saturn'];
+
+interface Tile {
+  type: string;
+}
+
+export default defineComponent({
+  name: 'GameBoard',
+  components: {
+    GameTile
+  },
+  props: {
+    width: {
+      type: Number,
+      default: 8 // default value if not provided
+    },
+    height: {
+      type: Number,
+      default: 8 // default value if not provided
+    }
+  },
+  setup(props) {
+    const boardData = ref<Array<Array<Tile>>>([]);
+
+    // Function to generate a random tile
+    const getRandomTile = (): Tile => {
+      const randomIndex = Math.floor(Math.random() * tileTypes.length);
+      return { type: tileTypes[randomIndex] };
+    };
+
+    // Function to generate the board with random tiles
+    const generateBoard = (): void => {
+      boardData.value = Array.from({ length: props.height }, () =>
+        Array.from({ length: props.width }, () => getRandomTile())
+      );
+    };
+
+    // Generate board on component mount
+    onMounted(generateBoard);
+
+    return { boardData, getRandomTile, generateBoard };
+  },
+  methods: {
+    getTileImage(tile: Tile): string {
+      // Return the path to the SVG based on the tile type
+      return require(`@/assets/${tile.type}.svg`);
+    },
+    handleTileClick(position: TilePosition) {
+      // Logic to handle tile click
+      console.log('Tile clicked:', position);
+    }
+  }
+});
+</script>
+
+<style scoped>
+.game-board {
+  display: flex;
+  flex-direction: column;
+}
+
+.board-row {
+  display: flex;
+}
+</style>
--- src/components/GameTile.vue
+++ src/components/GameTile.vue
@@ -1,1 +1,53 @@
 <!--GameTile.vue-->
+
+<template>
+    <div class="game-tile" @click="tileClicked">
+      <!-- Use an img tag to display the SVG -->
+      <img :src="tileImage" alt="Game Tile" />
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+ export interface TilePosition {
+    row: number;
+    col: number;
+  }
+  
+  export default defineComponent({
+    name: 'GameTile',
+    props: {
+      tileImage: String,
+      position: {
+        type: Object as () => TilePosition,
+        required: true
+      }
+    },
+    methods: {
+      tileClicked() {
+        this.$emit('tile-clicked', this.position);
+      }
+    }
+  });
+  </script>
+  
+  
+  <style scoped>
+  .game-tile {
+    /* Add styles for your tiles here */
+    width: 50px;
+    height: 50px;
+    border: 1px solid #ccc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+  
+  .game-tile img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+  </style>
+  
