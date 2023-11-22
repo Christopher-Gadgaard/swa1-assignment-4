26ad69f64b50084ca3984f9e3e7e589a2cbbea64
Chris309283 <Christopher.holmelund@gmail.com>
2023-11-22 17:55:17 +0100

    userprofile from vlad

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,56 +1,86 @@
 <template>
   <div>
-    <p>Hello, {{ state.username }}</p>
-    <button v-if="!state.showUpdate" @click="showUpdateField">Update Password</button>
-    <div v-if="state.showUpdate">
+    <p>Hello, {{ username }}</p>
+    <button v-if="!showUpdate" @click="showUpdateField">Update Password</button>
+    <div v-if="showUpdate">
       <input
         type="password"
-        v-model="state.newPassword"
+        v-model="newPassword"
         placeholder="Enter new password"
       />
       <button @click="updatePassword">Save</button>
       <button @click="cancelUpdate">Cancel</button>
     </div>
+    <p v-if="message">{{ message }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, reactive } from 'vue';
-import { useStore } from 'vuex';
+import { userService } from "@/services/userService";
+import { onMounted, ref } from "vue";
+import { useStore } from "vuex";
 
 export default {
   setup() {
     const store = useStore();
-    const playerId = store.state.user.id;
+    const username = ref("User");
+    const showUpdate = ref(false);
+    const newPassword = ref("");
+    const message = ref("");
+    const fetchUserData = async () => {
+      try {
+        const userId = store.state.user.user.id;
+        const token = store.state.user.token;
 
-    // Use reactive for an object holding multiple properties
-    const state = reactive({
-      username: "User", // Replace with dynamic data
-      showUpdate: false,
-      newPassword: "",
-    });
-
-    function showUpdateField() {
-      state.showUpdate = true;
-    }
-
-    function updatePassword() {
-      // Logic to update the password
-      state.showUpdate = false;
-      state.newPassword = "";
-    }
-
-    function cancelUpdate() {
-      state.showUpdate = false;
-      state.newPassword = "";
-    }
-
-    return { 
-      state, 
-      showUpdateField, 
-      updatePassword, 
-      cancelUpdate 
+        if (userId && token) {
+          const userData = await userService.getUserById(userId, token);
+          username.value = userData.username;
+        }
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+      }
     };
-  }
+
+    onMounted(fetchUserData);
+
+    const showUpdateField = () => {
+      showUpdate.value = true;
+    };
+
+    const updatePassword = async () => {
+      try {
+        const userId = store.state.user.user.id;
+        const token = store.state.user.token;
+
+        if (userId && token && newPassword.value) {
+          const updatedUserData = { password: newPassword.value };
+          await userService.updateUser(userId, updatedUserData, token);
+          message.value = "Password updated successfully!";
+          newPassword.value = "";
+          showUpdate.value = false;
+        } else {
+          message.value = "Missing user ID, token, or new password.";
+        }
+      } catch (error) {
+        console.error("Failed to update password:", error);
+        message.value = "Failed to update password. Please try again.";
+      }
+    };
+
+    const cancelUpdate = () => {
+      showUpdate.value = false;
+      newPassword.value = "";
+    };
+
+    return {
+      username,
+      showUpdate,
+      newPassword,
+      message,
+      showUpdateField,
+      updatePassword,
+      cancelUpdate,
+    };
+  },
 };
 </script>
