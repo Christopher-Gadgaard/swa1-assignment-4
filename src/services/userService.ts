1fc1f5904aef7bc4d77e2a2ec88d807ab70e0695
Vlad Lazar <lazar.vlad151@gmail.com>
2023-11-22 17:21:26 +0100

    user profile

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,1 +1,23 @@
-// user service
+import { apiService } from './apiService';
+
+export const userService = {
+  async getUserById(userId: string, token: string) {
+    try {
+      const data = await apiService.get(`/users/${userId}`, token);
+      return data;
+    } catch (error) {
+      console.error('Failed to fetch user:', error);
+      throw error;
+    }
+  },
+
+  async updateUser(userId: string, userData: object, token: string) {
+    try {
+      const data = await apiService.patch(`/users/${userId}`, token, userData);
+      return data;
+    } catch (error) {
+      console.error('User update failed:', error);
+      throw error;
+    }
+  }
+};
